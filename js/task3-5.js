06e25106eead8463ff176a7186e84988b9ad9698
JinHui <jh731448520@163.com>
2019-12-25 18:36:16 +0800

    [JinHui]{refactor}-{#3.5}:use function to package code

--- js/task3-5.js
+++ js/task3-5.js
@@ -1,45 +1,52 @@
+var todoList = document.getElementById("todo-list");
+var flag = "all";
+//读取数据库并初始化
+var active = getData("activeData");
+if (!active) {
+  active = {};
+}
+var complete = getData("completeData");
+if (!complete) {
+  complete = {};
+}
+var all = getData("allData");
+if (!all) {
+  all = {};
+}      
 window.onload = function() {
-  //读取数据库并初始化
-  var todoList = document.getElementById("todo-list");
-  var active = getData("activeData");
-  var flag = "all";
-  if (!active) {
-    active = {};
-  }
-  var complete = getData("completeData");
-  if (!complete) {
-    complete = {};
-  }
-  var all = getData("allData");
-  if (!all) {
-    all = {};
-  }        
   showTodoList(all,todoList);
-  //添加todolist并保存数据
-  var textInput = document.getElementsByClassName("text-input")[0];
-  var addButton = document.getElementsByClassName("pink-button")[0];
-  addButton.onclick = function () {
-    var textInputValue = textInput.value;
-      if (textInputValue) {
-        if (flag === "all") {
-          all[textInputValue] = 0;
-          active[textInputValue] = 0;
-          showTodoList(all,todoList);
-          saveData("allData",all);
-          saveData("activeData",active);
-        }else if (flag === "active"){
-          all[textInputValue] = 0;
-          active[textInputValue] = 0;
-          showTodoList(active,todoList);
-          saveData("allData",all);
-          saveData("activeData",active);
-        }else {
-          showTodoList(complete,todoList);
-        }
-        textInput.value = "";
-      }  
+  addTodoList();
+  changeTodoListStatus();
+  shiftTodoList();
+}
+//添加todolist并保存数据
+function addTodoList() {
+var textInput = document.getElementsByClassName("text-input")[0];
+var addButton = document.getElementsByClassName("pink-button")[0];
+addButton.onclick = function () {
+  var textInputValue = textInput.value;
+    if (textInputValue) {
+      if (flag === "all") {
+        all[textInputValue] = 0;
+        active[textInputValue] = 0;
+        showTodoList(all,todoList);
+        saveData("allData",all);
+        saveData("activeData",active);
+      }else if (flag === "active"){
+        all[textInputValue] = 0;
+        active[textInputValue] = 0;
+        showTodoList(active,todoList);
+        saveData("allData",all);
+        saveData("activeData",active);
+      }else {
+        showTodoList(complete,todoList);
+      }
+      textInput.value = "";
+    }  
   }
-  //改变todolist的状态并保存
+}
+//改变todolist的状态并保存
+function changeTodoListStatus() {
   var listContent =document.getElementById("list-content");
   listContent.addEventListener("click",function (event) {
     var listClick = event.target;
@@ -58,7 +65,9 @@ window.onload = function() {
     saveData("completeData",complete);  
     showTodoList(all,todoList);        
   });
-  //all,active,complete页面转换
+}       
+//all,active,complete页面转换
+function shiftTodoList() {
   var footer = document.getElementById("footer");
   footer.addEventListener("click",function (event) {
     var taskType = event.target.value;
@@ -75,43 +84,43 @@ window.onload = function() {
       default:
         showTodoList(all,todoList);
         flag = "all";               
-    }
+    }      
   })
-  //展示数据库里的todolist列表
-  function showTodoList(data,list) {
-    var grayBackground = false;
-    list.innerHTML = "";
-    if (data) {
-      for (var keys in data) {
-        var newList = document.createElement("li");
-        var listText = keys;
-        var listStatus = data[keys];                       
-        newList.innerHTML = 
-          '<input type="checkbox"  />' + 
-          '<span>' + 
-          listText +
-          '</span>';
-        if (listStatus) {
-          newList.style.textDecoration = "line-through";
-          newList.style.color = "rgb(128, 128, 128)";
-          newList.firstElementChild.setAttribute("checked","checked");
-        }else {
-          delete newList.firstElementChild.checked;
-        }
-        if (grayBackground) {
-          newList.style.backgroundColor = "rgb(244, 236, 236)";  
-        }
-        grayBackground = !grayBackground;
-        list.appendChild(newList);
+}
+//展示数据库里的todolist列表
+function showTodoList(data,list) {
+  var grayBackground = false;
+  list.innerHTML = "";
+  if (data) {
+    for (var keys in data) {
+      var newList = document.createElement("li");
+      var listText = keys;
+      var listStatus = data[keys];                  
+      newList.innerHTML = 
+        '<input type="checkbox"  />' + 
+        '<span>' + 
+        listText +
+        '</span>';
+      if (listStatus) {
+        newList.style.textDecoration = "line-through";
+        newList.style.color = "rgb(128, 128, 128)";
+        newList.firstElementChild.setAttribute("checked","checked");
+      }else {
+        delete newList.firstElementChild.checked;
       }
+      if (grayBackground) {
+        newList.style.backgroundColor = "rgb(244, 236, 236)";  
+      }
+      grayBackground = !grayBackground;
+      list.appendChild(newList);
     }
   }
-  //获取本地存储数据
-  function getData(type) {
-    return JSON.parse(localStorage.getItem(type));
-  }
-  //保存本地的数据
-  function saveData(type,data) {
-    window.localStorage.setItem(type,JSON.stringify(data));
-  }
+}
+//获取本地存储数据
+function getData(type) {
+  return JSON.parse(localStorage.getItem(type));
+}
+//保存本地的数据
+function saveData(type,data) {
+  window.localStorage.setItem(type,JSON.stringify(data));
 }
